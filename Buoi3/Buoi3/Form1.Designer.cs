446f6065e5a5259673b0a9aff60fb865ed31d401
Truong Ngoc Son <ngocson200346@gmail.com>
2024-09-05 14:39:49 +0700

    Lan3

--- Buoi3/Buoi3/Form1.Designer.cs
+++ Buoi3/Buoi3/Form1.Designer.cs
@@ -34,16 +34,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 77);
+            this.button1.Location = new System.Drawing.Point(124, 109);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(373, 109);
+            this.button2.Location = new System.Drawing.Point(218, 109);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
--- Buoi3/Buoi3/Form1.cs
+++ Buoi3/Buoi3/Form1.cs
@@ -16,5 +16,10 @@ namespace Buoi3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+        }
     }
 }
